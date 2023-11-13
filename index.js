09f68b7e5a3c581af96ce1ac4c6d0bf3676224dc
Vemula Karteek <vemula04@hotmail.com>
2023-11-14 00:15:03 +0530

    increase the size of the params

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 // const indexRouter = require('./routes/index');
@@ -8,8 +9,12 @@ const userRouter = require("./routes/user");
 const tokenRouter = require("./routes/tokens");
 const itemRouter = require("./routes/items");
 // Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(cors());
+
 // app.use('/', indexRouter);
 app.use('/', tenantRouter);
 app.use('/', userRouter);
